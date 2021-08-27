4deb4c0e427dd84a80cb40576867b10eee010529
blizzawang <blizzawang@163.com>
2021-08-27 22:11:23 +0800

    完善卖家申请功能，首页商品展示

---
+++ pages/shop/detail.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <!-- <pic-zoom :previewImg="smallImg" :zoomImg="bigImg"></pic-zoom> -->
+    <pic-zoom
+      :width="300"
+      :height="300"
+      :scale="3"
+      minIMGsrc="https://renren-auction.oss-cn-beijing.aliyuncs.com/shop-test/59ded626N7c4cb0a3.jpg"
+    ></pic-zoom>
+  </div>
+</template>
+<script>
+import PicZoom from "~/components/commons/PicZoom";
+export default {
+  components: {
+    PicZoom,
+  },
+  data() {
+    return {
+      data: {
+        imgUrl: "",
+      },
+    };
+  },
+};
+</script>
--- pages/welcome/index.vue
+++ pages/welcome/index.vue
@@ -78,118 +78,36 @@
         </div>
       </div>
       <div class="section_second_list">
-        <div class="swiper-container swiper_section_second_list_left">
-          <div class="swiper-wrapper">
-            <div class="swiper-slide">
-              <ul>
-                <li @click="toShopInfo()">
-                  <img
-                    src="~assets/images/section_second_list_img1.jpg"
-                    alt=""
-                  />
-                  <p>
-                    花王 (Merries) 妙而舒 纸尿裤 大号 L54片 尿不湿（9-14千克）
-                    （日本官方直采） 花王 (Merries) 妙而舒 纸尿裤 大号 L54片
-                    尿不湿（9-14千
-                  </p>
-                  <span>¥83.9</span><s>¥99.9</s>
-                </li>
-                <li>
-                  <img
-                    src="~assets/images/section_second_list_img2.jpg"
-                    alt=""
-                  />
-                  <p>华为mate9 4GB+32GB版 月光银 移动联通电信4G手机 双卡</p>
-                  <span>¥17.90</span><s>¥29.90</s>
-                </li>
-                <li>
-                  <img
-                    src="~assets/images/section_second_list_img3.jpg"
-                    alt=""
-                  />
-                  <p>超能 植翠低泡洗衣液（鲜艳亮丽）2kg/袋装（新老包装随机</p>
-                  <span>¥20.70</span><s>¥44.90</s>
-                </li>
-                <li>
-                  <img
-                    src="~assets/images/section_second_list_img4.jpg"
-                    alt=""
-                  />
-                  <p>长城（GreatWall）红酒 特选5年橡木桶解百纳干红葡萄酒 整</p>
-                  <span>¥399.00</span><s>¥599.00</s>
-                </li>
-                <li>
-                  <img
-                    src="~assets/images/section_second_list_img5.jpg"
-                    alt=""
-                  />
-                  <p>惠普(HP)暗影精灵2代Pro 15.6英寸游戏笔记本电脑(i5-7300H</p>
-                  <span>¥5999.00</span><s>¥6499.00</s>
-                </li>
-              </ul>
+        <!-- 每5个商品一行 -->
+        <div v-for="shopList in list" :key="shopList.index">
+          <div class="swiper-container swiper_section_second_list_left">
+            <div class="swiper-wrapper">
+              <div class="swiper-slide">
+                <ul>
+                  <li
+                    v-for="shop in shopList"
+                    :key="shop.shopId"
+                    @click="toShopInfo(shop.shopId)"
+                  >
+                    <img :src="shop.shopIndexImage" alt="" />
+                    <p>
+                      {{ shop.shopName }}
+                    </p>
+                    <span>¥{{ shop.shopStartPrice }}</span
+                    ><s>¥{{ shop.shopStartPrice + 20 }}</s>
+                  </li>
+                </ul>
+              </div>
             </div>
-            <div class="swiper-slide">
-              <ul>
-                <li>
-                  <img
-                    src="~assets/images/section_second_list_img6.jpg"
-                    alt=""
-                  />
-                  <p>
-                    Apple iMac 21.5英寸一体机（2017新款四核Core i5
-                    处理器/8GB内存/1TB/RP555显卡/4K屏 MNDY2CH/A） Apple iMac
-                    21.5英寸一体机（2017新款四核Core i5 处理
-                  </p>
-                  <span>¥9588.00</span><s>¥10288.00</s>
-                </li>
-                <li>
-                  <img
-                    src="~assets/images/section_second_list_img7.jpg"
-                    alt=""
-                  />
-                  <p>中柏（Jumper）EZpad 4S Pro 10.6英寸二合一平板电脑（X5 z</p>
-                  <span>¥848.00</span><s>¥899.00</s>
-                </li>
-                <li>
-                  <img
-                    src="~assets/images/section_second_list_img8.jpg"
-                    alt=""
-                  />
-                  <p>
-                    飞利浦（PHILIPS）电动牙刷HX6761/03亮白型成人充电式声波震动牙刷粉色
-                    飞利浦（PHILIPS）电动牙刷HX6761/03亮白型成人充电式声波
-                  </p>
-                  <span>¥379.00</span><s>¥698.00</s>
-                </li>
-                <li>
-                  <img
-                    src="~assets/images/section_second_list_img9.jpg"
-                    alt=""
-                  />
-                  <p>
-                    美的(Midea) 258升 变频智能三门冰箱 一级能效 风冷无霜 中门
-                  </p>
-                  <span>¥3088.00</span><s>¥3299.00</s>
-                </li>
-                <li>
-                  <img
-                    src="~assets/images/section_second_list_img10.jpg"
-                    alt=""
-                  />
-                  <p>【第二件减50元】蒙羊 内蒙古羔羊羊肋排 2.4斤</p>
-                  <span>¥99.90</span><s>¥199.00</s>
-                </li>
-              </ul>
+            <div class="swiper-button-prev second_list">
+              <p></p>
             </div>
-          </div>
-          <div class="swiper-button-prev second_list">
-            <p></p>
-          </div>
-          <div class="swiper-button-next second_list">
-            <p></p>
+            <div class="swiper-button-next second_list">
+              <p></p>
+            </div>
           </div>
         </div>
-        <ul class="section_second_list_right">
+        <!-- <ul class="section_second_list_right">
           <li>
             <img
               src="~assets/images/section_second_list_right_img.jpg"
@@ -206,7 +124,7 @@
             <p class="section_second_list_right_button_active"></p>
             <p></p>
           </div>
-        </ul>
+        </ul> -->
       </div>
     </div>
   </div>
@@ -218,6 +136,9 @@ import "~/assets/css/GL.css";
 export default {
   data() {
     return {
+      count: 0,
+      // shopList: [],
+      list: [],
       imgUrls: [
         require("~/assets/images/notable1.jpg"),
         require("~/assets/images/notable2.jpg"),
@@ -227,9 +148,50 @@ export default {
       ],
     };
   },
+  created() {
+    this.getShopInfo();
+  },
   methods: {
-    toShopInfo() {
-      alert(1);
+    // 获取即将开拍的拍品信息
+    getShopInfo() {
+      this.$axios.$get("/login/shop/list").then((response) => {
+        let list = response.data.list;
+        // 将拍品集合以每6个为一组进行分组
+        var tempList = [];
+        var newList = [];
+        var count = 0;
+        let flag = list.length % 6;
+        for (var i = 0; i < list.length; ++i) {
+          tempList.push(list[i]);
+          count++; // 计数器加1
+          if (count === 6) {
+            // 6个一组
+            // 将tempList放入newList
+            newList.push(tempList);
+            count = 0; // 重置计数器
+            tempList = []; // 清空tempList
+          }
+        }
+        tempList = [];
+        if (count !== 0) {
+          // 若count不等于0，则说明等分不均匀，还有余下count个数的拍品
+          for (var i = 0; i < count; ++i) {
+            tempList.push(list[list.length - 1 - i]);
+          }
+        }
+        newList.push(tempList.reverse());
+        this.list = newList;
+        console.log(newList);
+      });
+    },
+    // 点击某个拍品跳转至对应的详细信息页面
+    toShopInfo(shopId) {
+      this.$router.push({
+        path: "/shop/detail",
+        query: {
+          id: shopId,
+        },
+      });
     },
   },
 };
